d9eb7a2a61639f5d2b5b025b72cb291978482558
hiep2004 <buituanhiepvinhtuong@gmail.com>
2025-08-01 02:15:53 +0700

    connect mongodb and create env variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,30 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
+const MONGODB_URI =  'mongodb+srv://buituanhiepvinhtuong:goiNGu9y7wkBqL0Q@cluster0-tuanhiepdev.dthjauw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-TuanHiepDEV'
+
+const DATABASE_NAME = 'trello-database'
+
+import {MongoClient, ServerApiVersion}  from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+// Khởi tạo một đối tượng Client Instance để connect  tới MongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: ServerApiVersion.v1,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+// Kết nối tới Database MongoDB
+export const CONNECT_DB = async () => {
+// Gọi kết nối tới MongoDB Alats với URI đã được khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect();
+
+// Kết nối thành công thì lấy ra Database theo tên và gán ngược nó lại vào biến  trelloDatabaseInstance ở trên của chúng ta
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) 
+    throw new Error('Database not connected')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,32 +1,42 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+/* eslint-disable no-console */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import {CONNECT_DB, GET_DB} from './config/mongodb.js'
 
-const app = express()
+const START_SEVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
-
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
-
-app.listen(port, hostname, () => {
+  const hostname = 'localhost'
+  const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+  })
+  app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+    console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+  })
+}
+
+// Chỉ khi kết nối thành công tới MongoDB thì mới khởi động server
+// Immediately Invoked Function Expression (IIFE) to handle async/await at the top level
+(async () => {
+  try {
+    console.log('Connecting to MongoDB...')
+    await CONNECT_DB()
+    console.log('Conected to MongoDB successfully')
+    START_SEVER()
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error)
+    process.exit(1) // Exit the process with a failure code
+  }
+})()
+
+// CONNECT_DB()
+//   .then(() => {
+//     console.log('Conected to MongoDB successfully')
+//   })
+//   .then(() => START_SEVER())
+//   .catch(error => {
+//     console.error('Error connecting to MongoDB:', error)
+//     process.exit(1) // Exit the process with a failure code
+//   })
