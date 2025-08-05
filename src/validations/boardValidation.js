7e3e60495c7242bbf1b2733df2456609835ba4cd
hiep2004 <buituanhiepvinhtuong@gmail.com>
2025-08-05 15:36:51 +0700

    validation with joi

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,40 @@
+import Joi from 'joi';
+import { StatusCodes } from 'http-status-codes'
+import e from 'express'
+
+const createNew = async (req, res, next) => {
+    const correctCondition = Joi.object({
+        title: Joi.string().required().min(3).max(50).trim().strict().messages({
+            'any.required': 'Title is required',
+            'string.empty': 'Title cannot be empty',
+            'string.min': 'Title must be at least 3 characters long',
+            'string.max': 'Title must not exceed 50 characters',
+            'string.trim': 'Title cannot have leading or trailing spaces'
+        }),
+        description: Joi.string().required().min(3).max(50).trim().strict().messages({
+            'any.required': 'Description is required',
+            'string.empty': 'Description cannot be empty',
+            'string.min': 'Description must be at least 3 characters long',
+            'string.max': 'Description must not exceed 50 characters',
+            'string.trim': 'Description cannot have leading or trailing spaces'
+        })
+    })
+
+    try {
+        await correctCondition.validateAsync(req.body, { abortEarly: false });
+        res.status(StatusCodes.CREATED).json({
+            message: 'Board created successfully'
+        });
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+            errors: new Error(error).message
+        })
+    }
+
+}
+
+export const boardValidation = {
+    createNew
+}
